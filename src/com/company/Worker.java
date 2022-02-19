5ed56dd2eb2e65b2433f64d5d1f4016c80736188
Marat Sattarov <sattarovmk@yandex.ru>
2022-02-19 22:21:49 +0500

    Initial commit

---
+++ src/com/company/Worker.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+        this.callback = callback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is unsuccessful");
+                continue;
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
