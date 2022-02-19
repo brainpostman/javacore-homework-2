1e390567bc4bb3d43465d88ccf4d5ab7b69aac47
Marat Sattarov <sattarovmk@yandex.ru>
2022-02-19 22:22:47 +0500

    Code styling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	OnTaskDoneListener listener = System.out::println;
-    OnTaskErrorListener errorListener = System.out::println;
-    Worker worker = new Worker(listener, errorListener);
-    worker.start();
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
     }
 }
